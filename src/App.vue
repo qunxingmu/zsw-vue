89ba2d49f571534abaa53ee031ea6dcf90f2df18
conquer_mu <14152552+mu-qun-xing@user.noreply.gitee.com>
2024-10-26 15:09:27 +0800

    引入element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-button>这是一个按钮</el-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
